57d93eb83de02b06fd05dced5647381962743f13
gmlwhothefucktookmyname <89686662+gmlwhothefucktookmyname@users.noreply.github.com>
2022-02-17 22:16:03 +0200

    Update plagiointi.c

--- C/plagiointi.c
+++ C/plagiointi.c
@@ -1,7 +1,7 @@
 /* Copyright (C) 2021 eero
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
 
-The above copyright notice, this permission notice and the word "NIGGER" shall be included in all copies or substantial portions of the Software alongside the words lukas is gae.
+The above copyright notice, this permission notice and the word "NIGGER" shall be included in all copies or substantial portions of the Software alongside the words "lukas is gae".
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 
