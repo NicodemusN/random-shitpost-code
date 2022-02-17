f93c6334d8d78f7cc9790e44804b3efd14e87757
Tarmomymman <99886422+Tarmomymman@users.noreply.github.com>
2022-02-17 16:02:16 +0200

    Update nubmerios.py

--- python/nubmerios.py
+++ python/nubmerios.py
@@ -61,27 +61,100 @@ def random():
          print("Väärä luku syötetty")
       else:
          if iarvaus == karvaus:
-            print("Oikea arvaus! Sinä ja tietokone arvasitte", karvaus, print("."))
+            print("Oikea arvaus! Sinä ja tietokone arvasitte", karvaus, ".")
          else:
             print("Väärä arvaus. Sinä arvasit", iarvaus, "ja tietokone arvasi", karvaus)
    elif rankys == "n":
          print("Näkemiin.")
 
-def testi(fname, lname):
-   print("testi")
+def nimi(fname, lname):
    print(fname, lname)
+   fnum = len(fname)
+   fnum = int(fnum)
+   lnum = len(lname)
+   lnum = int(lnum)
+   print(fnum, "ja", lnum, "kirjainta. Yhteensä kirjaimia", lnum+fnum)
+   for letter in fname:
+      print(letter)
 
-# Kuuluu ylläolevaan testiin. Tulee aina testattaessa.
-testi("Jorma", "Ollila")
+def nimi(fname, tname, lname):
+   print(fname, tname, lname)
+   fnum = len(fname)
+   fnum = int(fnum)
+   tnum = len(tname)
+   tnum = int(tnum)
+   lnum = len(lname)
+   lnum = int(lnum)
+   print(fnum, ",", tnum, "ja", lnum, "kirjainta. Yhteensä kirjaimia", lnum+tnum+fnum)
+   for letter in fname:
+      print(letter)
 
-akysymys = input("Kirjautuminen [k], numerot [n] vai numeroarvaus [na]?")
-if akysymys == "k":
-   kirjautu()
-elif akysymys == "n":
-   numeroita()
-elif akysymys == "na":
-   random()
-elif akysymys == "t":
-   testi()
-else:
-   print("Väärä kirjain, palautetaan...")
+def nimi(fname, lname):
+   print(fname, lname)
+   fnum = len(fname)
+   fnum = int(fnum)
+   lnum = len(lname)
+   lnum = int(lnum)
+   print(fnum, "ja", lnum, "kirjainta. Yhteensä kirjaimia", +lnum+fnum)
+   for letter in fname:
+      print(letter)
+
+# Tää on rikki, tein koko jutun ilman mitään testausta :D:D:D
+def laskin():
+   import math
+   mkysymys = input("Matemaattinen toimitus: [nj] neliöjuuri, [k] kertoma, [s] sini, [c] kosini, [t] tangentti, [ar] asteet radiaaneiksi, [ra] radiaanit asteiksi.")
+   if mkysymys == "nj":
+      njkys = input("Sijoita luku, jonka neliöjuuren haluat ratkaista: ")
+      print(math.sqrt(njkys))
+   elif mkysymys == "k":
+      kkys = input("Sijoita luku, jonka kertoman haluat ratkaista: ")
+      print(math.factorial(kkys))
+   elif mkysymys == "s":
+      skys = input("Sijoita luku, jonka sinin haluat ratkaista: ")
+      print(math.sin(skys))
+   elif mkysymys == "c":
+      ckys = input("Sijoita luku, jonka kosinin haluat ratkaista: ")
+      print(math.cos(ckys))
+   elif mkysymys == "t":
+      tkys = input("Sijoita lunku, jonka tangentin haluat ratkaista: ")
+      print(math.tan(tkys))
+   elif mkysymys == "ar":
+      arkys = input("Sijoita luku, jonka haluat muuttaa asteista radiaaneiksi: ")
+      print(math.degrees(arkys))
+   elif mkysymys == "ra":
+      rakys = input("Sijoita luku, jonka haluat muuttaa radiaaneista asteiksi: ")
+      print(math.radians(rakys))
+   
+
+def aky():
+   akysymys = input("Kirjautuminen [k], numerot [n], nimi [ni], laskin [l] vai numeroarvaus [na]?")
+   if akysymys == "k":
+      kirjautu()
+   elif akysymys == "n":
+      numeroita()
+   elif akysymys == "l":
+      laskin()
+   elif akysymys == "na":
+      random()
+   elif akysymys == "ni":
+      fname = input("Etunimi: ")
+      lname = input("Sukunimi: ")
+      tnimi = input("Onko sinulla toinen nimi? Kyllä [y] / ei [n]")
+      if tnimi == "y":
+         tname = input("Toinen nimi: ")
+         nimi(fname, tname, lname)
+      elif tnimi == "n":
+         nimi(fname, lname)
+      else:
+         print("Väärä kirjain, palautetaan...")
+   else:
+      print("Väärä kirjain, palautetaan...")
+
+jk = input("Haluatko käyttää MemelSoftarea? Kyllä [y] / ei [n]")
+
+while jk == "y":
+   aky()
+   jk = input("Haluatko jatkaa? Kyllä [y] / ei [n]")
+
+if jk != "y":
+   print("Näkemiin...")
