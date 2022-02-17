8595db569bfc98aaad787ef5fd99c42bf0020a6d
NicodemusN <87670335+NicodemusN@users.noreply.github.com>
2022-02-17 18:30:31 +0000

    created main.c

---
+++ C/main.c
@@ -0,0 +1,13 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include<stdio.h>
+
+void main()
+{
+int a,b,c;
+printf("Please enter any two numbers: \n");
+scanf("%d %d", &a, &b);
+c =  a + b;
+printf("The addition of two number is: %d", c);
+}
