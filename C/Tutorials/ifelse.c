7c2ce34f10acd879fe442445ad2873513df2c347
NicodemusN <87670335+NicodemusN@users.noreply.github.com>
2022-02-17 18:44:41 +0000

    created ifelse.c

---
+++ C/Tutorials/ifelse.c
@@ -0,0 +1,21 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+//https://www.online-ide.com/online_c++_ide
+
+#include<stdio.h>
+
+void main()
+{
+    int x = 9;
+    int y = 8;
+    printf("Is x higher than y?\n");
+    
+    if(x > y)
+    {
+       printf("true");
+    }
+    else
+    {
+       printf("false");
+    }
+}
